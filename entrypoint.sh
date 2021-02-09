857d994cac0c5be172b2d73a231044af0d0edcaf
John Duimovich <jduimovich@gmail.com>
2021-02-08 19:25:20 -0500

    upfate script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,35 @@
 if [ "$GITHUB_REPOSITORY" = "" ]
 then
-   echo "GITHUB_REPOSITORY does not exist"
+   echo "Missing Environment variable GITHUB_REPOSITORY"
+   exit 1
 else
-   echo "GITHUB_REPOSITORY dxists" as $GITHUB_REPOSITORY 
+   echo "GITHUB_REPOSITORY" is $GITHUB_REPOSITORY 
 fi
-
 if [ "$1" = "" ]
 then
-   echo "token does not exist"
+   echo "token does not exist" 
+   exit 1
 else
-   echo "token exists" as $1 
+   echo "Github Token Set "
 fi
-
 if [ "$2" = "" ]
 then
-   echo "event_type does not exist"
+   echo "event_type missing"
+   exit 1
 else
-   echo "event_type exists" as $2 
+   echo "event_type fired:" $2 
 fi
-
-URL=https://api.github/com/repos/$GITHUB_REPOSITORY/dispatches
- 
-D="{'event_type': '$2'}"
-echo "$D" >payload.json 
-H1="Authorization: Bearer $1" 
-
-echo curl --request POST  --header $H1 --data  @payload.json --url $URL
-curl --request POST  --header $H1 --data  @payload.json --url $URL
-  
-RESULT="RAN"
+URL=https://api.github.com/repos/$GITHUB_REPOSITORY/dispatches
+echo '{"event_type":' '"'$2'"}' >payload.json 
+echo "Authorization: Bearer $1" >header 
+curl -X POST --header @header --data  @payload.json $URL
+retVal=$?
+if [ "$retVal" = 0 ]
+then
+   RESULT="ok"
+else
+   RESULT="error"
+fi
 echo "::set-output name=result::$RESULT"
 
 
